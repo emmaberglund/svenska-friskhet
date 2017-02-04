f6ce8e9a6a12fd2ee1c340f2664fd52626be6070
Emma Berglund <emma@ourinterest.se>
2017-02-04 16:42:08 +0100

    Front page fixes

--- front-page.php
+++ front-page.php
@@ -5,8 +5,9 @@
         while (have_posts()) : the_post(); ?>
 <?php global $post;
     //get the content of the current post inside the loop
-    $content = get_the_content();
+    $content = get_the_content_with_formatting();
 ?>
+
 <div class="header-image" style="background-image:url(<?php echo wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'header-image')[0]; ?>);">
 </div>
 <?php
@@ -41,11 +42,12 @@ $pages = get_pages(array('child_of'=> $post->ID ,'sort_order'=> 'asc', 'sort_col
             <div class="row">
             <?php echo''; // Här kan man skriva en rubrik
             while ($my_query->have_posts()) : $my_query->the_post(); ?>
-                    <div class="three columns news" style="background-image:url(<?php echo wp_get_attachment_image_src(get_post_thumbnail_id($page['ID']), 'large')[0]; ?>);">
-                        <svg viewBox="0 0 180 320" preserveAspectRatio="none">
+                    <div class="three columns news">
+                        <div class="news-image" style="background-image:url(<?php echo wp_get_attachment_image_src(get_post_thumbnail_id($page['ID']), 'large')[0]; ?>);"></div>
+                        <!-- <svg viewBox="0 0 180 320" preserveAspectRatio="none">
                             <path d="M0,0C0,0,0,180,0,180C0,180,90,130,90,130C90,130,180,180,180,180C180,180,180,0,180,0C180,0,0,0,0,0" style="fill:#ffffff"></path>
                             <path d="M0,0C0,0,0,50,0,50C0,50,90,70,90,70C90,70,180,50,180,50C180,50,180,0,180,0C180,0,0,0,0,0" style="fill:#ffffff"></path>
-                            <desc>Created with Snap</desc><defs></defs></svg>
+                            <desc>Created with Snap</desc><defs></defs></svg> -->
                         <div class="caption">
                         <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
                         <?php
@@ -145,43 +147,9 @@ $pages = get_pages(array('child_of'=> $post->ID ,'sort_order'=> 'asc', 'sort_col
                     }
                     ?>
                 </div>
-        </div>
-        <?php
-        // Set up the objects needed
-        $my_wp_query = new WP_Query();
-        $all_wp_pages = $my_wp_query->query(array('post_type' => 'page', 'posts_per_page' => -1 ));
-        // Get the page as an Object
-        $cases =  get_page_by_title('Cases');
-        $casesPage = get_post($cases);
-        // Filter through all pages and find Portfolio's children
-        $cases_children = get_page_children( $cases->ID, $all_wp_pages );
-        ?>
-        <div class="cases-container">
-            <div class="row">
-                    <?php
-                    $count=1;
-                    foreach ($cases_children as $case) {
-                        if($count <= 8){
-                        ?>
-                    <div class="case-item">
-                        <!--<div class="case-overlay"></div>-->
-                        <div class="case-img" style="background-image:url(<?php echo wp_get_attachment_image_src(get_post_thumbnail_id($case->ID), 'medium')[0]; ?>);" >
-                        </div>
-                        <!--<h2 class="case-title"><?php //echo $case->post_title; ?></h2>-->
-                    </div>
-                    <?php if($count % 4 == 0) {
-                        ?>
             </div>
-                  <div class="row">
-                      <?php }
-                      $count++;
-                  }
-              }
-                  wp_reset_query();
-                  ?>
-                </div>
-    </div>
-</div>
+
+        </div>
 <?php
     endwhile;
 else :
